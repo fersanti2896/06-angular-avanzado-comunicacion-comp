374fd524599129e3519a115338bc0a43cbae5277
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-31 17:40:19 -0500

    Usando ngModel para capturar datos en progress.component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from '../shared/shared.module';
     PagesComponent
   ],
   imports: [
-    CommonModule, 
+    CommonModule,
+    FormsModule, 
     SharedModule,
     RouterModule
   ], 
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: [ './progress.component.css' ]
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent  {
+  progreso: number = 40;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get getProcentaje() {
+    return `${ this.progreso }%` 
   }
 
+  cambiarValor( valor: number ) {
+    this.progreso = this.progreso + valor; 
+    
+    if ( this.progreso < 0 ) {
+      this.progreso = 0;
+    }
+
+    if ( this.progreso > 100 ) {
+      this.progreso = 100;
+    }
+
+     
+  }
 }
