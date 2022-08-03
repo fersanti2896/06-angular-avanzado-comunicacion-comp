67e919ff0e86261023b65b4ee61f7a0b1d99a07c
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-02 20:11:41 -0500

    Implementando gráficas en dona.component y grafica1.component

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
+  public labels1: string[]  = [ 'CETES', 'SuperTasas', 'Kubo Financiero', 'Finsus', 'GBM+' ];
+  public data1: number[]    = [ 2634.71, 6727.87, 3689.26, 3199.43, 2486.67 ]; 
+  public colors1: string[]  = [ '#6BA4FA', '#FF4838', '#41EB7A', '#FAE5D4', '#FFE038' ]
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public labels2: string[]  = [ 'Efectivo', 'Mercado Pago', 'Upsi Vale', 'Banco Azteca' ];
+  public data2: number[]    = [ 300, 723.32, 24.99, 1 ]; 
+  public colors2: string[]  = [ '#6BA4FA', '#FF4838', '#41EB7A', '#FF9C26', '#5AFADF' ]
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,11 +4,12 @@ import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router'
 
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
